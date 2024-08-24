ea2a5412cc697d782a12608d65604f09c19559fa
Tamakuz <jovangithub@gmail.com>
2024-08-24 22:56:42 +0700

    remote

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const password = "R19yJzPgRXIwERQx";
 
 const LocalUri = `mongodb://localhost:27017/animehatori`;
-const RemoteUri = `mongodb://tamakuz:${password}@cluster0-shard-00-00.onqly.mongodb.net:27017,cluster0-shard-00-01.onqly.mongodb.net:27017,cluster0-shard-00-02.onqly.mongodb.net:27017/?ssl=true&replicaSet=atlas-8a28iu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+const RemoteUri = `mongodb://tamakuz:${password}@cluster0-shard-00-00.onqly.mongodb.net:27017,cluster0-shard-00-01.onqly.mongodb.net:27017,cluster0-shard-00-02.onqly.mongodb.net:27017/animehatori?ssl=true&replicaSet=atlas-8a28iu-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 
 if (!LocalUri || !RemoteUri) {
   throw new Error(
