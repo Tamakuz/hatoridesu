471c013ec808457d224d0a2c1b60e17da929dd99
Tamakuz <jovangithub@gmail.com>
2024-09-02 06:47:54 +0700

    Update List and User pages, and modify anime route handling

---
+++ app/user/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserPage = () => {
+  return (
+    <div className="container mx-auto p-4 flex flex-col items-center justify-center min-h-screen bg-black">
+      <h1 className="text-4xl font-bold mb-4 text-white">User Page</h1>
+      <p className="text-red-500 mb-4 ">This page is still under development.</p>
+    </div>
+  );
+};
+
+export default UserPage;
